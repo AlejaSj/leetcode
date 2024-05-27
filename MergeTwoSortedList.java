1f5ba90b9cfc6c521d05d8e0a8ffed37643a1d5b
AlejandroSJ <alejandrosj015@gmail.com>
2024-05-26 18:38:13 -0600

    sixth challenge

---
+++ MergeTwoSortedList.java
@@ -0,0 +1,25 @@
+class MergeTwoList {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        ListNode list3 = new ListNode(0);
+        ListNode current = list3;
+        while (list1!=null && list2!=null) {
+            if (list1.val<=list2.val) {
+                
+                current.next = list1;
+                list1 = list1.next;
+            }else{
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        if (list1!=null) {
+            current.next = list1;
+        }else{
+            current.next = list2;
+        }
+        
+        return list3.next;   
+    }
+}
